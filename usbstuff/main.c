e0ad7fbc6b4214b3d86496142f49d91f2bc6b467
John Heaton <jheaton.dev@gmail.com>
2014-02-08 20:06:51 -0500

    Initial Commit

---
+++ usbstuff/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  usbstuff
+//
+//  Created by John Heaton on 2/8/14.
+//  Copyright (c) 2014 John Heaton. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
